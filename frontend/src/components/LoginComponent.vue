f122e2eaf503cb303341717f204272a6e47f57a4
Aranza-Ricalde <studioghibli8888@gmail.com>
2024-05-23 13:51:08 -0600

    creating all-round component

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,67 +1,21 @@
 <template>
-  <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
-
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Email</div>
-
-      <v-text-field
-        density="compact"
-        placeholder="Email"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-        v-model="email"
-      ></v-text-field>
-
-      <div
-        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
-      >
-        Password
-      </div>
-
-      <v-text-field
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-        v-model="password"
-      ></v-text-field>
-
-      <v-btn
-        block
-        class="mb-8"
-        color="blue"
-        size="large"
-        variant="tonal"
-        @click="login"
-      >
-        Log In
-      </v-btn>
-
-      <v-card-text class="text-center">
-        <router-link to="/signup" class="text-blue text-decoration-none">
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
-    </v-card>
-    <v-snackbar v-model="snackbar" :color="snackbarColor" multi-line>
-      {{ snackbarMessage }}
-      <v-btn color="white" text @click="snackbar = false" class="ml-auto">Close</v-btn>
-    </v-snackbar>
-  </div>
+  <div class="d-flex align-center">
+    <div style="position: relative; width: 65%; height: 950px;">
+      <v-img src="../assets/cool-background.svg" alt="Imagen de nosotros" cover width="100%" height="100%"></v-img>
+      <v-text class="text-h2 text-center text-white font-weight-bold" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">UserSphere</v-text>
+    </div>
+    <div class="mx-auto">
+      
+        <SignUpForm></SignUpForm>
+    
+      <v-snackbar v-model="snackbar" :color="snackbarColor" multi-line>
+        {{ snackbarMessage }}
+        <v-btn color="white" text @click="snackbar = false">Close</v-btn>
+      </v-snackbar>
+    </div>
+  </div>   
 </template>
+
 <script>
 import axios from "axios";
 
